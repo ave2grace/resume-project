868e6144cd234a4f4f269898e6d5b3887bab8e48
ave2grace <ave2grace@gmail.com>
2025-09-17 09:17:36 -0500

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,13 @@
+//define variables
+let myName= "Avery Barlow";
+let title= "Server at The Cheesecake Factory";
+let education= "Oral Roberts University";
+let experience= "Team Member at Chick-fil-a, Host at Texas Roadhouse, Social Media Manager, Server at The Rooftop";
+let skills = ["Attention to Detail", "Communication", "Conflict Resolution", "Creativity", "POS operation", "Social Media Management", "Salesmanship"];
+
+//connect variables to the HTML (ligaments)
+document.getElementById("my-name").textContent = myName;
+document.getElementById("title").textContent = title;
+document.getElementById("education").textContent = education;
+document.getElementById("experience").textContent = experience;
+document.getElementById("skills").textContent = skills;
